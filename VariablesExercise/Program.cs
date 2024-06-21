3789de8c73e980139921a8bb6557fb5591b12fa1
Martaze <martazebcoder@gmail.com>
2024-06-21 05:29:15 -0400

    Value Types Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Martaze";
+            int myAge = 24;
+            char myInitial = 'M';
+            bool myAnswer = true;
+            double myMoney = 5.50;
+            decimal myInvestment = 55.50m;
+
+            Console.WriteLine($"Hello, my name is {myName}, and I am {myAge} years old.");
+            Console.WriteLine($"Of course my favorite letter is {myInitial}, it's literally the first letter of my name!");
+            Console.WriteLine($"{myAnswer} as that may be, it also starts the word money! I had to deposit {myMoney} extra dollars into my cashapp yesterday.");
+            Console.WriteLine($"I really don't like having to use cashapp. I had to deposit {myInvestment} because cashapp takes a percentage of what you deposit, and I had to make sure that it was an even amount.");
         }
     }
 }
